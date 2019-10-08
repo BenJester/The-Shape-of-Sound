ecb9360b51f3b7d382e9eeeb87391361c69396cb
BenJester <jichu.yang1998@gmail.com>
2019-10-08 11:52:24 -0400

    the name of life

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public static Audio Instance { get; private set; }
+
+    public GameObject A3;
+    public GameObject D3;
+    public GameObject E3;
+
+    void Awake()
+    {
+        if (Instance == null) { Instance = this; }
+        else { Destroy(gameObject); }
+    }
+
+    public void PlayNote(int note)
+    {
+        GameObject noteObject = null;
+        switch (note)
+        {
+            case 3:
+                noteObject = Instantiate(E3);
+                break;
+            case 2:
+                noteObject = Instantiate(D3);
+                break;
+            case 6:
+                noteObject = Instantiate(A3);
+                break;
+        }
+        noteObject.transform.parent = transform;
+    }
+}
---
+++ Assets/Scripts/SongClip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SongClip : MonoBehaviour
+{
+    public List<int> notes;
+    int index;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            PlayNext();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+
+    }
+
+    void PlayNext()
+    {
+        if (index >= notes.Count) return;
+        Audio.Instance.PlayNote(notes[index]);
+        index += 1;
+    }
+}
